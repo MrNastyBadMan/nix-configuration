25cdf8ecdda61e9deaaad45e961280d6d01b888d
MrNastyBadMan <ropcalwill@gmail.com>
2025-04-28 12:10:00 +0100

    nix colors working, currently with gruvbox thing, will do for now cause idk wtf im doing help

--- home.nix
+++ home.nix
@@ -37,7 +37,6 @@
     pkgs.networkmanagerapplet
     pkgs.brightnessctl
     pkgs.hypridle
-    pkgs.flavours
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -53,13 +52,39 @@
     # '')
   ];
 
-  colorScheme = inputs.nix-colors.colorSchemes.gruvbox-dark-medium; #lib.schemeFromYaml "test-scheme" (builtins.readFile /home/wrc/.local/share/flavours/base16/schemes/generated/2d.yaml);
+  colorScheme = inputs.nix-colors.colorSchemes.gruvbox-dark-medium;
 
   programs.kitty = {
     enable = true;
     settings = {
       background_opacity = 0.75;
       confirm_os_window_close = 0;
+
+      background = "#${config.colorScheme.palette.base00}";
+      foreground = "#${config.colorScheme.palette.base05}";
+      selection_background = "#${config.colorScheme.palette.base03}";
+      selection_foreground = "#${config.colorScheme.palette.base05}";
+
+      cursor = "#${config.colorScheme.palette.base05}";
+      cursor_text_color = "#${config.colorScheme.palette.base00}";
+
+      color0  = "#${config.colorScheme.palette.base00}";
+      color1  = "#${config.colorScheme.palette.base08}";
+      color2  = "#${config.colorScheme.palette.base0B}";
+      color3  = "#${config.colorScheme.palette.base0A}";
+      color4  = "#${config.colorScheme.palette.base0D}";
+      color5  = "#${config.colorScheme.palette.base0E}";
+      color6  = "#${config.colorScheme.palette.base0C}";
+      color7  = "#${config.colorScheme.palette.base05}";
+
+      color8  = "#${config.colorScheme.palette.base02}";
+      color9  = "#${config.colorScheme.palette.base08}";
+      color10 = "#${config.colorScheme.palette.base0B}";
+      color11 = "#${config.colorScheme.palette.base0A}";
+      color12 = "#${config.colorScheme.palette.base0D}";
+      color13 = "#${config.colorScheme.palette.base0E}";
+      color14 = "#${config.colorScheme.palette.base0C}";
+      color15 = "#${config.colorScheme.palette.base07}";
     };
   };
 
