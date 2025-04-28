6b9f09301bfde236e624c148465a8e16e7fa4c66
MrNastyBadMan <ropcalwill@gmail.com>
2025-04-28 12:50:58 +0100

    moved kitty to separate nix file

--- home.nix
+++ home.nix
@@ -4,6 +4,7 @@
   imports = [
     ./hyprland.nix
     ./waybar.nix
+    ./kitty.nix
     inputs.nix-colors.homeManagerModules.default
   ];
 
@@ -53,40 +54,6 @@
   ];
 
   colorScheme = inputs.nix-colors.colorSchemes.gruvbox-dark-medium;
-
-  programs.kitty = {
-    enable = true;
-    settings = {
-      background_opacity = 0.75;
-      confirm_os_window_close = 0;
-
-      background = "#${config.colorScheme.palette.base00}";
-      foreground = "#${config.colorScheme.palette.base05}";
-      selection_background = "#${config.colorScheme.palette.base03}";
-      selection_foreground = "#${config.colorScheme.palette.base05}";
-
-      cursor = "#${config.colorScheme.palette.base05}";
-      cursor_text_color = "#${config.colorScheme.palette.base00}";
-
-      color0  = "#${config.colorScheme.palette.base00}";
-      color1  = "#${config.colorScheme.palette.base08}";
-      color2  = "#${config.colorScheme.palette.base0B}";
-      color3  = "#${config.colorScheme.palette.base0A}";
-      color4  = "#${config.colorScheme.palette.base0D}";
-      color5  = "#${config.colorScheme.palette.base0E}";
-      color6  = "#${config.colorScheme.palette.base0C}";
-      color7  = "#${config.colorScheme.palette.base05}";
-
-      color8  = "#${config.colorScheme.palette.base02}";
-      color9  = "#${config.colorScheme.palette.base08}";
-      color10 = "#${config.colorScheme.palette.base0B}";
-      color11 = "#${config.colorScheme.palette.base0A}";
-      color12 = "#${config.colorScheme.palette.base0D}";
-      color13 = "#${config.colorScheme.palette.base0E}";
-      color14 = "#${config.colorScheme.palette.base0C}";
-      color15 = "#${config.colorScheme.palette.base07}";
-    };
-  };
 
   programs.hyprlock.enable = true;
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
