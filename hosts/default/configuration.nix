e62d20b176a9a1c1fad09e6836e7c044032e0f2d
MrNastyBadMan <ropcalwill@gmail.com>
2026-03-06 21:14:19 +0000

    added mullvad

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -177,7 +177,8 @@
   # };
 
   # List services that you want to enable:
-
+  services.mullvad-vpn.enable = true;
+  services.mullvad-vpn.package = pkgs.mullvad-vpn;
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
